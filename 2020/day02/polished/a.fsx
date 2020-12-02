9792160ab8e8893f3f879f2006135f2bab3318d0
Benjamin Geiger <github@bgeiger.net>
2020-12-02 01:36:57 -0500

    For fun, go back and polish.
    There are better ways to complete the tasks, and it's easier to find
    them when I'm not under time pressure. So, I figured I'd go back and
    clean up my code a bit, using better approaches. That way I'll know how
    to do it next time.

---
+++ 2020/day02/polished/a.fsx
@@ -0,0 +1,38 @@
+// vim: set et ts=4 sw=4 list :
+
+open System
+open System.Text.RegularExpressions
+
+let readLines filePath = System.IO.File.ReadLines filePath
+let input = readLines "input.txt"
+
+
+// Parse the input line.
+let extractValues (s : string) =
+    let pattern = @"^(\d+)-(\d+) (.): (.*)$"
+    let regex = Regex pattern
+
+    let m = regex.Match s
+
+    let lower = int m.Groups.[1].Value
+    let upper = int m.Groups.[2].Value
+    let target = m.Groups.[3].Value.[0]
+    let password = m.Groups.[4].Value
+
+    lower, upper, target, password
+
+
+// Is this password valid, given the request?
+let isValidPassword (low, high, target, password : string) =
+    let count = Seq.filter (fun c -> c = target) password |> Seq.length
+    count >= low && count <= high
+
+
+let result = input 
+                |> Seq.map extractValues
+                |> Seq.filter isValidPassword
+                |> Seq.length
+
+printfn "%A" result
+
+
--- 2020/template.fsx
+++ 2020/template.fsx
@@ -1,5 +1,7 @@
 // vim: set et ts=4 sw=4 list :
 
+open System
+
 let readLines filePath = System.IO.File.ReadLines filePath
 let input = readLines "input.txt"
 
