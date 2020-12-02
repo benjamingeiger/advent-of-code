4e1c68d0684aa2d9ca281bf86c56f3d2e5cc8489
Benjamin Geiger <github@bgeiger.net>
2020-12-02 00:49:24 -0500

    Two errors canceled each other out.

--- 2020/day02/b.fsx
+++ 2020/day02/b.fsx
@@ -11,10 +11,10 @@ let splitIntoParts (s : string) =
     let range = subparts.[0].Split '-' |> Array.map int
     let target = subparts.[1].[0]
 
-    range.[0], range.[1], target, parts.[1]
+    range.[0], range.[1], target, parts.[1].Trim()
 
 let isValidPassword (low, high, target, password : string) =
-    (password.[low] = target) <> (password.[high] = target)
+    (password.[low - 1] = target) <> (password.[high - 1] = target)
 
 let result = input 
                 |> Seq.map splitIntoParts
