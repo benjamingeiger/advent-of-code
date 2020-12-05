2178c869052877f46002b6a743232991f1e09871
Benjamin Geiger <github@bgeiger.net>
2020-12-05 00:15:06 -0500

    Day 5. (632/876)

---
+++ 2020/day05/b.fsx
@@ -0,0 +1,29 @@
+// vim: set et ts=4 sw=4 list :
+
+open System
+
+let readLines filePath = System.IO.File.ReadLines filePath
+let input = readLines "input.txt"
+
+let parseSeat (row, col) c =
+    match c with
+    | 'B' -> (row * 2 + 1, col)
+    | 'F' -> (row * 2, col)
+    | 'L' -> (row, col * 2)
+    | 'R' -> (row, col * 2 + 1)
+    | _ -> (row, col)
+
+let passes =
+    input
+    |> Seq.map (Seq.fold parseSeat (0, 0))
+    |> Seq.map (fun (r, c) -> r * 8 + c)
+
+let maxPass = Seq.max passes
+let minPass = Seq.min passes
+
+let openSeats =
+    [minPass..maxPass]
+    |> Seq.filter (fun x -> not (Seq.exists ((=) x) passes))
+
+printfn "%A" openSeats
+
