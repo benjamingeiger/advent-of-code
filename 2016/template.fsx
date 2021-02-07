41ed0e3a10348f7e56b2ccdac6b783f25fe3497f
Benjamin Geiger <github@bgeiger.net>
2021-02-07 05:28:04 -0500

    Modify the template to add something useful.

--- 2016/template.fsx
+++ 2016/template.fsx
@@ -1,9 +1,14 @@
 // vim: set et ts=4 sw=4 list :
 
 open System
+open System.Text.RegularExpressions
+
+// borrowed from https://theburningmonk.com/2016/12/advent-of-code-fsharp-day-10/
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
 
 let readLines filePath = System.IO.File.ReadLines filePath
 let input = readLines "input.txt"
-
-
 
