87f67022b862c7cedebeb99b4cdd3b94fb497211
Benjamin Geiger <bgeiger@webstaurantstore.com>
2021-12-01 10:02:54 -0500

    2021 day 1. Minor cleanup.

--- 2021/day01/a.fsx
+++ 2021/day01/a.fsx
@@ -1,20 +1,16 @@
 // vim: set et ts=4 sw=4 list :
 
 open System
-open System.Collections.Generic 
 
 let readLines filePath = System.IO.File.ReadLines filePath
 let input = readLines "input.txt" |> Seq.map Int32.Parse |> Seq.cache
 
-let pairs =
+let result =
     input
     |> Seq.windowed 2
-
-let descendingPairs =
-    pairs
     |> Seq.filter (fun p -> p.[1] > p.[0])
     |> Seq.length
 
-printfn "%A" descendingPairs
+printfn "%A" result
 
 
--- 2021/day01/b.fsx
+++ 2021/day01/b.fsx
@@ -1,22 +1,18 @@
 // vim: set et ts=4 sw=4 list :
 
 open System
-open System.Collections.Generic 
 
 let readLines filePath = System.IO.File.ReadLines filePath
 let input = readLines "input.txt" |> Seq.map Int32.Parse |> Seq.cache
 
-let windowSums =
+let result =
     input
     |> Seq.windowed 3
     |> Seq.map (Array.sum)
-
-let pairs =
-    windowSums
     |> Seq.windowed 2
     |> Seq.filter (fun p -> p.[1] > p.[0])
     |> Seq.length
 
-printfn "%A" pairs
+printfn "%A" result
 
 
