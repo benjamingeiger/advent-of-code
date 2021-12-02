1bd3e14742a1b1b69a02852b353b3e250f89ae7f
Benjamin Geiger <bgeiger@webstaurantstore.com>
2021-12-02 13:07:38 -0500

    Export stuff to a utility script.

--- 2021/day02/a.fsx
+++ 2021/day02/a.fsx
@@ -1,15 +1,10 @@
 // vim: set et ts=4 sw=4 list :
 
 open System
-open System.Text.RegularExpressions
 
-// borrowed from https://theburningmonk.com/2016/12/advent-of-code-fsharp-day-10/
-let (|Regex|_|) pattern input =
-    let m = Regex.Match(input, pattern)
-    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
-    else None
+#load "../utils.fsx"
+open Utils
 
-let readLines filePath = System.IO.File.ReadLines filePath
 let input = readLines "input.txt"
 
 type Direction =
