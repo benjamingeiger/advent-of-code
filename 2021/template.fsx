99fa656f1f2124cb7ca1279c784f47f21813efc5
Benjamin Geiger <bgeiger@webstaurantstore.com>
2021-12-02 00:19:38 -0500

    Update template to include useful Regex active pattern.

---
+++ 2021/template.fsx
@@ -0,0 +1,29 @@
+// vim: set et ts=4 sw=4 list :
+
+open System
+open System.Collections.Generic 
+open System.Text.RegularExpressions
+
+// borrowed from https://theburningmonk.com/2016/12/advent-of-code-fsharp-day-10/
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+let enumerateSeq s = Seq.zip (Seq.initInfinite id) s
+
+let memoize (f : 'a -> 'b) =
+    let dict = new Dictionary<'a, 'b>()
+
+    let memoizedFunc (input : 'a) =
+        match dict.TryGetValue(input) with
+        | true, x -> x
+        | false, _ ->
+            let answer = f input
+            dict.Add(input, answer)
+            answer
+
+    memoizedFunc
+
+let readLines filePath = System.IO.File.ReadLines filePath
+let input = readLines "input.txt"
