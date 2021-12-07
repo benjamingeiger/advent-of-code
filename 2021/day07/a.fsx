9c2a97591251beb315318a02727dc31261784216
Benjamin Geiger <bgeiger@webstaurantstore.com>
2021-12-07 00:31:34 -0500

    2021 day 7. Leaderboard is down.

---
+++ 2021/day07/a.fsx
@@ -0,0 +1,20 @@
+// vim: set et ts=4 sw=4 list :
+
+open System
+
+#load "../utils.fsx"
+open Utils
+
+let input = readLines "input.txt" |> Seq.head |> (fun (s : string) -> s.Split(",")) |> Seq.map int
+
+let calculateFuel crabs pos =
+    crabs
+    |> Seq.map ((-) pos >> abs)
+    |> Seq.sum
+
+let result =
+    [(Seq.min input)..(Seq.max input)]
+    |> Seq.map (calculateFuel input)
+    |> Seq.min
+
+printfn "%A" result
---
+++ 2021/day07/b.fsx
@@ -0,0 +1,23 @@
+// vim: set et ts=4 sw=4 list :
+
+open System
+
+#load "../utils.fsx"
+open Utils
+
+let input = readLines "input.txt" |> Seq.head |> (fun (s : string) -> s.Split(",")) |> Seq.map int
+
+let calculateFuel crabs pos =
+    let offset x y = (abs (x - y))
+    let fuel x y = (offset x y) * ((offset x y) + 1) / 2
+
+    crabs
+    |> Seq.map (fuel pos)
+    |> Seq.sum
+
+let result =
+    [(Seq.min input)..(Seq.max input)]
+    |> Seq.map (calculateFuel input)
+    |> Seq.min
+
+printfn "%A" result
