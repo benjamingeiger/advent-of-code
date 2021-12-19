da5ac7ff5629def0110930e18137cbf4bc8c58ee
Benjamin Geiger <bgeiger@webstaurantstore.com>
2021-12-19 04:13:26 -0500

    2021 day 19. 1517/1509. Way too slow.
    a.fsx and b.fsx take about 12 minutes to run on complete input.
    There has to be a way to speed it up, but I'm not sure how.

---
+++ 2021/day19/b.fsx
@@ -0,0 +1,155 @@
+// vim: set et ts=4 sw=4 list :
+
+open System
+
+#load "../utils.fsx"
+open Utils
+
+let input = readLines "input.txt" |> List.ofSeq
+
+let parseInput input =
+    input
+    |> splitSeq ((=) "")
+    |> Seq.map Seq.tail
+    |> Seq.map (Seq.choose (function
+        | Regex "^(-?\d+),(-?\d+),(-?\d+)$" [x; y; z] ->
+            Some (int x, int y, int z)
+        | x ->
+            printfn "Unparseable line: '%s'" x
+            None))
+    |> Seq.map List.ofSeq
+    |> List.ofSeq
+
+
+let allRotations =
+    let aboutX (x, y, z) = (x, z, -y)
+    let aboutY (x, y, z) = (-z, y, x)
+    let aboutZ (x, y, z) = (y, -x, z)
+
+    [
+        id;
+        aboutX;
+        aboutY;
+        aboutZ;
+        aboutX >> aboutX;
+        aboutX >> aboutY;
+        aboutX >> aboutZ;
+        aboutY >> aboutX;
+        aboutY >> aboutY;
+        aboutZ >> aboutY;
+        aboutZ >> aboutZ;
+        aboutX >> aboutX >> aboutX;
+        aboutX >> aboutX >> aboutY;
+        aboutX >> aboutX >> aboutZ;
+        aboutX >> aboutY >> aboutX;
+        aboutX >> aboutY >> aboutY;
+        aboutX >> aboutZ >> aboutZ;
+        aboutY >> aboutX >> aboutX;
+        aboutY >> aboutY >> aboutY;
+        aboutZ >> aboutZ >> aboutZ;
+        aboutX >> aboutX >> aboutX >> aboutY;
+        aboutX >> aboutX >> aboutY >> aboutX;
+        aboutX >> aboutY >> aboutX >> aboutX;
+        aboutX >> aboutY >> aboutY >> aboutY
+    ]
+
+let allBeaconRotations scans = allRotations |> List.map (fun f -> List.map f scans)
+
+(*let beaconMatch (x1, y1, z1) (x2, y2, z2) = (x1 - y1 = x2 - y2) && (x1 - z1 = x2 - z2) && (y1 - z1 = y2 - z2)*)
+
+let beaconShift (offsetx, offsety, offsetz) (x, y, z) = (x - offsetx, y - offsety, z - offsetz)
+
+let beaconOffset (x1, y1, z1) (x2, y2, z2) = (x1 - x2, y1 - y2, z1 - z2)
+let beaconOffset2 (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
+
+let attemptScannerMatch scans1 scans2 =
+    let findBeaconDifferences scans =
+        List.allPairs scans scans
+        |> List.filter (fun (x, y) -> x <> y)
+        |> List.map (fun ((x1, y1, z1), (x2, y2, z2)) -> ((x2 - x1, y2 - y1, z2 - z1), ((x1, y1, z1), (x2, y2, z2))))
+
+    let scan1BeaconPairs = findBeaconDifferences scans1
+    let scan2BeaconPairs = findBeaconDifferences scans2
+
+    let scanResult =
+        scan2BeaconPairs
+        |> List.filter (fun (diff, (p1, p2)) -> scan1BeaconPairs |> List.map fst |> List.contains diff)
+        |> List.allPairs scan1BeaconPairs
+        |> List.choose (fun ((d1, (p11, p12)), (d2, (p21, p22))) ->
+            if d1 <> d2 then None
+            else Some (beaconOffset p11 p21))
+        |> List.countBy id
+        |> function
+            | [] -> None
+            | xs ->
+                xs
+                |> List.maxBy snd
+                |> Some
+
+    match scanResult with
+    | None -> None
+    | Some (probablePosition, countOfMatchingBeacons) ->
+        if countOfMatchingBeacons >= 132 then
+            Some (probablePosition, List.map (fun s -> beaconOffset2 probablePosition s) scans2)
+        else
+            None
+
+let findMatchingScanners existingBeaconSets scanSet =
+    existingBeaconSets
+    |> List.tryPick (fun s ->
+        scanSet
+        |> List.tryPick (fun ss -> attemptScannerMatch s ss))
+
+let attachScanners init scanSets =
+    let rec go (existingScanners, unmatched) scanSets =
+        printfn "Iterating: %A existing scanners, %A unmatched scan sets" (List.length existingScanners) (List.length scanSets)
+
+        let existingBeaconSets = List.map snd existingScanners
+
+        scanSets
+        |> List.fold (fun (existingScanners', unmatched') scanSet ->
+            match findMatchingScanners existingBeaconSets scanSet with
+            | Some scans ->
+                printfn "Found a scanner at %A" (fst scans)
+                (scans :: existingScanners', unmatched')
+            | None -> (existingScanners', scanSet :: unmatched')) (existingScanners, [])
+        |> function
+            | (scanners, []) -> scanners
+            | (scanners, unmatched'') ->
+                go (scanners, []) unmatched''
+
+    go (init, []) scanSets
+
+let manhattan (x1, y1, z1) (x2, y2, z2) = abs (x2 - x1) + abs (y2 - y1) + abs (z2 - z1)
+
+let result =
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+
+    let parsed = parseInput input
+
+    let beacons = List.head parsed
+
+    let scanSets =
+        parsed
+        |> List.tail
+        |> List.map allBeaconRotations
+
+    let scanResults = attachScanners [(0, 0, 0), beacons] scanSets
+
+    let allScanners =
+        scanResults
+        |> List.map fst
+
+    let furthestScanners =
+        List.allPairs allScanners allScanners
+        |> List.map (fun (x, y) -> manhattan x y)
+        |> List.max
+
+    let returnValue = furthestScanners
+
+    stopwatch.Stop()
+    printfn "Elapsed milliseconds: %f" stopwatch.Elapsed.TotalMilliseconds
+
+    returnValue
+
+printfn "%A" result
