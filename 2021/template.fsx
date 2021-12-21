e982e8cff9bd090660d4bccddd60dbd2a0f1ee0b
Benjamin Geiger <bgeiger@webstaurantstore.com>
2021-12-20 23:52:18 -0500

    Update template a bit.

--- 2021/template.fsx
+++ 2021/template.fsx
@@ -5,4 +5,24 @@ open System
 #load "../utils.fsx"
 open Utils
 
-let input = readLines "input.txt"
+let input = readLines "input.txt" |> List.ofSeq
+
+let parseInput input =
+    input
+
+
+
+let result =
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+
+    let parsed = parseInput input
+
+    let returnValue =
+        parsed
+
+    stopwatch.Stop()
+    printfn "Elapsed milliseconds: %f" stopwatch.Elapsed.TotalMilliseconds
+
+    returnValue
+
+printfn "%A" result
