70ca449bc555e0965f5850a25d145271ab9ac76f
Benjamin Geiger <bgeiger@webstaurantstore.com>
2022-12-01 17:08:11 -0500

    Add user agent to get-input.sh.

--- 2022/get-input.sh
+++ 2022/get-input.sh
@@ -2,4 +2,4 @@
 
 SCRIPTDIR=$(dirname $0)
 
-wget https://adventofcode.com/2022/day/$1/input --header "Cookie: $(cat $SCRIPTDIR/cookie.txt)" -O input.real.txt && cp input.real.txt input.txt
+wget https://adventofcode.com/2022/day/$1/input --user-agent "https://github.com/benjamingeiger/advent-of-code/blob/main/2022/get-input.sh" --header "Cookie: $(cat $SCRIPTDIR/cookie.txt)" -O input.real.txt && cp input.real.txt input.txt
