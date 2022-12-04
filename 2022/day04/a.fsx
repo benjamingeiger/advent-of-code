ce9c9d4b291461b086e540ab345699cc36c78a6e
Benjamin Geiger <github@bgeiger.net>
2022-12-04 00:11:41 -0500

    2022 day 04. 2229/2961.

---
+++ 2022/day04/a.fsx
@@ -0,0 +1,23 @@
+// vim: set et ts=4 sw=4 list :
+
+// open System
+
+#load "../utils.fsx"
+open Utils
+
+let rawInput = readLines "input.txt" |> List.ofSeq
+
+let parseInput rawInput =
+    rawInput
+    |> List.choose (function
+        | Regex "^(\d+)-(\d+),(\d+)-(\d+)$" [a1; b1; a2; b2] ->
+            Some ((int a1, int b1), (int a2, int b2))
+        | _ -> None)
+
+let run input =
+    input
+    |> List.filter (fun ((a1, b1), (a2, b2)) ->
+        (a1 <= a2 && b1 >= b2) || (a2 <= a1 && b2 >= b1))
+    |> List.length
+
+doProcess parseInput run rawInput
